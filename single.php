e75c2e9d496c38bf9bdeb77271959fa2f0075c99
小西 大祐 <skydai1151@gmail.com>
2015-03-02 19:29:19 +0900

    作品の情報を出力するようにした

--- single.php
+++ single.php
@@ -1,23 +1,44 @@
 <?php get_header(); ?>
     <main role="main">
+      <?php 
+      if (have_posts()) :
+        while (have_posts()) :
+          the_post();
+      ?>
       <div class="row">
         <div class="large-12 columns">
           <img alt="作品のスクリーンショット" height="510" src="<?php echo get_stylesheet_directory_uri(); ?>/images/portfolio-ss__large.png" width="917" />
-          <h1 class="production-title">ココに作品のタイトルが表示されます。</h1>
-          <p>URL: <a href="#">http://google.com</a></p>
+          <h1 class="production-title"><?php the_title(); ?></h1>
+          <?php 
+          $key="url";
+          ?>
+          <p>URL: <a href="<?php echo get_post_meta($post->ID, $key, true);?>">
+          <?php
+          echo get_post_meta($post->ID, $key, true);
+          ?>
+          </a></p>
         </div>
       </div>
       <div class="row">
         <div class="concept large-8 columns">
           <h2 class="comment-title">Comment</h2>
-          <p>the_content();制作背景工夫した・こだわった点</p>
-          <p>あれば成果</p>
+          <?php the_content(); ?>
         </div>
         <aside class="large-4 columns">
           <h2 class="data-title">Data</h2>
           <dl class="category">
             <dt>カテゴリー</dt>
-            <dd><a href="#">Webデザイン</a></dd>
+            <dd>
+            <?php
+            $postcat = get_the_category();
+            
+            if ($postcat) {
+              foreach($postcat as $cat) {
+                echo $cat->name;
+              }
+            }
+            ?>
+            </dd>
           </dl>
           <dl>
             <dt>使用した技術</dt>
@@ -35,18 +56,32 @@
           </dl>
           <dl>
             <dt>制作期間</dt>
-            <dd>2014年9月-10月</dd>
+            <dd>
+            <?php 
+            $key="制作期間";
+            echo get_post_meta($post->ID, $key, true);
+            ?>
+            </dd>
           </dl>
           <dl>
             <dt>担当箇所</dt>
-            <dd>すべて</dd>
+            <dd>
+            <?php 
+            $key="担当箇所";
+            echo get_post_meta($post->ID, $key, true);
+            ?>
+            </dd>
           </dl>
         </aside>
       </div>
       <div class="row">
         <div class="large-12 columns">
-          <a class="button radius" href="#">戻る</a>
+          <a class="button radius" href="<?php bloginfo('url'); ?>">戻る</a>
         </div>
       </div>
+      <?php
+        endwhile;
+      endif;
+      ?>
     </main>
 <?php get_footer(); ?>
