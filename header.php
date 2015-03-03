2dcb6c1c066f9d603b098be58dd153d251c0e443
小西 大祐 <skydai1151@gmail.com>
2015-03-03 21:34:41 +0900

    OGP情報を出力するようにした

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
     <meta name="description" content="<?php bloginfo('description'); ?>">
     <link href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" rel="stylesheet" />
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
+    <?php get_template_part('header_ogp'); ?>
   </head>
   <body>
     <header class="groval-header">
