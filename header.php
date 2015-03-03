14299f4c2e25abfda145b2d77a8d839cfc98d0df
小西 大祐 <skydai1151@gmail.com>
2015-03-03 21:31:37 +0900

    ページの説明を出力するようにした

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <title><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta name="description" content="<?php bloginfo('description'); ?>">
     <link href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" rel="stylesheet" />
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
   </head>
