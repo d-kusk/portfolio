a6f1f0cad2a964d3e49ad1a62300f4bfe08fd6fe
Daisuke <skydai1151@gmail.com>
2015-07-21 01:32:05 +0900

    ogp情報の出力をやめた

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
     <link href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" rel="stylesheet" />
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
     <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-    <?php get_template_part('header_ogp'); ?>
+    <?php // get_template_part('header_ogp'); ?>
     <!--[if lt IE 9]>
     script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"
     script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"
