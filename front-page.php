3f3cd10f7b0bbd556d8c77cf2b07b59e029385b5
Daisuke <skydai1151@gmail.com>
2015-08-23 14:41:57 +0900

    トップページの作品の並びを12カラムで中央に寄せるコードを追加した

--- front-page.php
+++ front-page.php
@@ -123,45 +123,49 @@
       <section id="portfolio" class="main-section">
         <h2 class="section-title">Portfolio</h2>
         <p>これまでに作ったWebサイトやポスターなど</p>
-        <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-3">
-        <?php
-        if (have_posts()) :
-          while (have_posts()) :
-            the_post();
-        ?>
-          <li class="portfolio-item" <?php post_class();?>>
-            <a class="portfolio-item__thumbnail" href="<?php the_permalink(); ?>">
+        <div class="row">
+          <div class="small-12 columns">
+            <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-3">
               <?php
-              if (has_post_thumbnail())
-                the_post_thumbnail('medium');
+              if (have_posts()) :
+                while (have_posts()) :
+                  the_post();
               ?>
-              <div class="hover-items">
-                <p class="hover-item">more<i class="fa fa-angle-right"></i></p>
-              </div>
-            </a>
-            <p class="portfolio-item__title">
-              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            </p>
-            <div class="tech">
-              <p>使用技術</p>
-              <ul class="inline-list">
-                <?php
-                $posttags = get_the_tags();
-
-                if ($posttags) {
-                  foreach($posttags as $tag) {
-                    echo '<li>' . $tag->name . '</li>';
-                  }
-                }
-                ?>
-              </ul>
-            </div>
-          </li>
-        <?php
-          endwhile;
-        endif;
-        ?>
-        </ul>
+              <li class="portfolio-item" <?php post_class();?>>
+                <a class="portfolio-item__thumbnail" href="<?php the_permalink(); ?>">
+                  <?php
+                  if (has_post_thumbnail())
+                    the_post_thumbnail('medium');
+                  ?>
+                  <div class="hover-items">
+                    <p class="hover-item">more<i class="fa fa-angle-right"></i></p>
+                  </div>
+                </a>
+                <p class="portfolio-item__title">
+                  <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </p>
+                <div class="tech">
+                  <p>使用技術</p>
+                  <ul class="inline-list">
+                    <?php
+                    $posttags = get_the_tags();
+    
+                    if ($posttags) {
+                      foreach($posttags as $tag) {
+                        echo '<li>' . $tag->name . '</li>';
+                      }
+                    }
+                    ?>
+                  </ul>
+                </div>
+              </li>
+              <?php
+                endwhile;
+              endif;
+              ?>
+            </ul>
+          </div>
+        </div>
         <div class="row">
           <div class="btn-more-area small-7 small-centered medium-3 columns">
             <?php
